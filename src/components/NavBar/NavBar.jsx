dab94a42435cd5f918d1a1d6114e258de455ddf7
polino <polinovskyoleksandr1303@gmail.com>
2026-02-10 15:02:05 +0100

    added things

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,16 +13,16 @@ const NavBar = () => {
         <nav>
             { user ? (
                 <ul>
-                    <li><Link to=''>Home</Link></li>
+                    <li><Link to='/'>Home</Link></li>
                     <li><Link to=''>Community</Link></li>
-                    <li><Link to=''>{user.username}'s Diary</Link></li>
+                    <li><Link to=''>{user.displayedname}'s Diary</Link></li>
                     <li><Link to='' onClick= { handleSignOut }>Sign Out</Link> </li>
                 </ul>
             ) : (
                 <ul>
                     <li><Link to=''>Community</Link></li>
                     <li><Link to='/sign-in'>Sign In</Link></li>
-                    <li><Link to='/sign-out'>Sign Out</Link></li>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
                 </ul>
             )}
         </nav>
